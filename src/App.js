04a5ce1ac3aacb9c5f50702bba3f12bc90d483c1
newt <newtykins@outlook.com>
2021-01-07 20:48:49 +0000

    Add todos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
+import shortid from 'shortid';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
+  constructor() {
+    super();
 
-export default App;
+    this.state = {
+      todos: [],
+      value: '',
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.addTodo = this.addTodo.bind(this);
+  }
+
+  componentDidMount() {
+    const todos = [];
+    Object.keys(localStorage).forEach(id => {
+      todos.push({ id, value: localStorage[id] });
+    });
+    this.setState(todos);
+    console.log(todos, this.state);
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+  
+  addTodo() {
+    const { value, todos } = this.state;
+    const id = shortid.generate();
+    const newTodos = todos.concat({ id, value });
+
+    this.setState({ todos: newTodos, value: '' });
+    localStorage.setItem(id, value);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+        <input type="submit" value="Submit" onClick={this.state.value === '' ? null : this.addTodo} />
+      </div>
+    );
+  }
+}
